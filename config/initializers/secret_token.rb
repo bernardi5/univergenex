e635ff13136fc93d810a7b8e08bf420649240213
Enrico Bernardi <enrico.bernardi@gmail.com>
2012-03-09 20:18:54 +0100

    Just started

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Univergenex::Application.config.secret_token = 'c81da5d5c0da2a86b5587b0628cffdc724f3cf9c6ad68147d7f89adfa51c07c11564f11b86393ef7149cfe96ad3262cfd42eac2d0b75c2159ad36b28e9f6b771'
